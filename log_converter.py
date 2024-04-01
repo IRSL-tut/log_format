b718e04623f44de2c1d90461d2e3f4f8b7d9781c
Yohei Kakiuchi <kakiuchi.yohei.sw@tut.jp>
2024-04-01 17:40:18 +0900

    add -d, -p arguments to log_converter.py

--- log_converter.py
+++ log_converter.py
@@ -17,16 +17,18 @@ def expand_str_to_list (input_str):
     else:
         return input_str
 
-def main(): 
+def main():
     # args
     parser = argparse.ArgumentParser(description='Convert data from mc_rtc log to hrpsys log')
     parser.add_argument('-f', type=str, help='input file', metavar='file', required=True)
+    parser.add_argument('-d', type=str, help='output dir', metavar='file')
+    parser.add_argument('-p', type=str, help='prefix', metavar='file')
     parser.add_argument('--plot', type=str, help='plot configure file', metavar='file')
     args = parser.parse_args()
 
     # load log file
     mc_rtc_log = mc_log_ui.read_log(args.f)
-    
+
     # mc_rtc logのtopic名を取得
     topic_list = {}
     # mc_rtc topic list
@@ -46,16 +48,23 @@ def main():
                 else:
                     topic_list[key[0]] = [keys]
 
+    if args.p is None:
+        prefix=args.f.split('.')[0]
+    else:
+        prefix = args.p
+    if args.d is not None:
+        prefix = args.d + '/' + prefix.split('/')[-1]
+
     df_list = {}
     for topic in topic_list.keys():
         df = pd.DataFrame(index=mc_rtc_log["t"], columns=topic_list[topic])
         for key in topic_list[topic]:
             if key in mc_rtc_log.keys():
                 df[key] = mc_rtc_log[key]
-                
+
         if not df.empty:
-            df.to_csv(args.f.split('.')[0] + "." + topic , sep=' ', na_rep= 0, index=True, header=False)
+            df.to_csv(prefix + '.' + topic , sep=' ', na_rep= 0, index=True, header=False)
             df_list[topic] = df
-    
+
 if __name__ == "__main__":
     main()
